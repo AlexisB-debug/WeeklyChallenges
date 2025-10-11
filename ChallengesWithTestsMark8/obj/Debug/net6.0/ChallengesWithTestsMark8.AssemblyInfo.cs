741a1bc95c33b21754c3f82d3e0ed1e8ec0f47ce
Alexis Boegner <alexisboegner@gmail.com>
2025-10-11 10:54:49 -0400

    CountOfPositiveOddsBelowNumber

--- ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
+++ ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd48f94b1f96d0af80e60aada880d5f4216465b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e67959c09ad139b27ce73bfd492d07d73ba92c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

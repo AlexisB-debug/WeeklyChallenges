b84d1fa05aa836ea47d45ce32b6d03127d76b9bf
Alexis Boegner <alexisboegner@gmail.com>
2025-11-28 17:13:20 -0500

    Completed ChallengesSet05

--- ChallengesWithTestsMark8/obj/Debug/net9.0/ChallengesWithTestsMark8.AssemblyInfo.cs
+++ ChallengesWithTestsMark8/obj/Debug/net9.0/ChallengesWithTestsMark8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c4a8e92c0d535e6c2987996d1f802ca7e9178a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98bb9d2cbeb068a6c2a33e6dd9e23873b00bbb17")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
